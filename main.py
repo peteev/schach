9fd735c0e69fb46f6dc33470d359ef75506582db
peteev <petegames@gmx.de>
2022-12-18 15:39:46 +0100

    Klassen für Figuren erstellt

--- main.py
+++ main.py
@@ -4,27 +4,104 @@ import pygame
 from PIL import Image
 import sys
 
-# Klassen (https://www.w3schools.com/python/python_classes.asp)
+# Klassen (https://www.w3schools.com/python/python_classes.asp; https://www.w3schools.com/python/python_inheritance.asp)
+class Figur:
+    def __init__(self, farbe):
+        self.farbe = farbe
+        self.posX = 0
+        self.posY = 0
+        
+class Bauer(Figur):
+    def __init__(self, farbe, num):
+        super().__init__(farbe)
+        self.bauer_startpos(farbe,num)
+        
+        # Umwandlung zu anderen Figuren sobald Bauer die andere Spielseite erreicht (funktioniert nicht)
+        self.umwandlung = False
+        
+        
+        match self.farbe, self.posY:
+            case "w",8:
+                self.umwandlung = True
+            case "b",1:
+                self.umwandlung = True
+            case _:
+                self.umwandlung = False
+        
+        
 
-class Bauer():
-    def __init__(self):
-        pass
-class Springer():
-    def __init__(self):
-        pass
-class Laeufer():
-    def __init__(self):
-        pass
-class Turm():
-    def __init__(self):
-        pass
-class Dame():
-    def __init__(self):
-        pass
-class Koenig():
-    def __init__(self):
-        pass
+        
+    def bauer_startpos(self, farbe, num):
+        # Startpositionen der verschiedenen Bauern 
+        self.posX = num
+        if farbe == "w":
+            self.posY = 2
+        else:
+            self.posY = 7
+        
+class Springer(Figur):
+    def __init__(self, farbe, num):
+        super().__init__(farbe)
+    
+    def springer_startpos(self, farbe, num):
+        if num == 1:
+            self.posX = 2
+        else:
+            self.posX = 7
+        if farbe == "w":
+            self.posY = 1
+        else:
+            self.posY = 8
+        
+class Laeufer(Figur):
+    def __init__(self, farbe, num):
+        super().__init__(farbe)
+        
+    def laeufer_startpos(self, farbe, num):
+        if num == 1:
+            self.posX = 3
+        else:
+            self.posX = 6
+        if farbe == "w":
+            self.posY = 1
+        else:
+            self.posY = 8
+        
+class Turm(Figur):
+    def __init__(self, farbe, num):
+        super().__init__(farbe)
+    
+    def turm_startpos(self, farbe, num):
+        if num == 1:
+            self.posX = 1
+        else:
+            self.posX = 8
+        if farbe == "w":
+            self.posY = 1
+        else:
+            self.posY = 8
+        
+class Dame(Figur):
+    def __init__(self, farbe):
+        super().__init__(farbe)
+        
+    def dame_startpos(self, farbe):
+        self.posX = 4
+        if farbe == "w":
+            self.posY = 1
+        else:
+            self.posY = 8
+            
+class Koenig(Figur):
+    def __init__(self, farbe):
+        super().__init__(farbe)
 
+    def koenig_startpos(self, farbe):
+        self.posX = 5
+        if farbe == "w":
+            self.posY = 1
+        else:
+            self.posY = 8
 
 
 
@@ -87,7 +164,6 @@ while running:
     screen.blit(text , (width/2+50,height/2))
 
     pygame.display.update()
-
 
 
 
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+
+print(wB1.umwandlung, wB1.posY, wB1.farbe)
